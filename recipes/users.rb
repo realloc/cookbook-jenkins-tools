4f0dbddecbf041d687499d06695f691b0c9d33b7
Stanislav Bogatyrev <realloc@realloc.spb.ru>
2015-03-31 21:18:51 +0300

    Add users from attributes

---
+++ recipes/users.rb
@@ -0,0 +1,23 @@
+#
+# Cookbook Name:: jenkins-tools
+# Recipe:: users
+
+node['jenkins']['server']['users'].each do |user|
+
+  if user.is_a?(Hash)
+    name = user['username']
+    full_name = user['full_name'] if user['full_name']
+    email = user['email'] if user['email']
+    password = user['password'] if user['password']
+    public_keys = user['public_keys'] if user['public_keys']
+  else
+    name = user
+  end
+
+  jenkins_user name do
+    full_name if full_name
+    email if email
+    password if password
+    public_keys if public_keys
+  end
+end
