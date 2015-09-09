17861724ef6bb0e2a15858a5f606f9aa527c3b8c
Stanislav Bogatyrev <realloc@realloc.spb.ru>
2015-09-09 18:58:18 +0300

    Change attributes location

--- attributes/job-dsl.rb
+++ attributes/job-dsl.rb
@@ -1,4 +1,4 @@
-default['jenkins-tools']['job-dsl']['seed_job_file'] = 'job-dsl_seed.xml'
-default['jenkins-tools']['job-dsl']['repo'] = 'https://bitbucket.org/realloc_spb_ru/jenkins-jobs.git'
-default['jenkins-tools']['job-dsl']['branch'] = '*/master'
-default['jenkins-tools']['job-dsl']['targets'] = '*.job'
+default['jenkins']['server']['job-dsl']['seed_job_file'] = 'job-dsl_seed.xml'
+default['jenkins']['server']['job-dsl']['repo'] = 'https://bitbucket.org/realloc_spb_ru/jenkins-jobs.git'
+default['jenkins']['server']['job-dsl']['branch'] = '*/master'
+default['jenkins']['server']['job-dsl']['targets'] = '*.job'
--- recipes/job-dsl.rb
+++ recipes/job-dsl.rb
@@ -6,16 +6,16 @@
 
 seed_job_xml = File.join(
   Chef::Config[:file_cache_path],
-  node['jenkins-tools']['job-dsl']['seed_job_file']
+  node['jenkins']['server']['job-dsl']['seed_job_file']
 )
 
 template seed_job_xml do
   source 'seed-job.xml.erb'
   variables(
     {
-      :repo => node['jenkins-tools']['job-dsl']['repo'],
-      :branch => node['jenkins-tools']['job-dsl']['branch'],
-      :targets => node['jenkins-tools']['job-dsl']['targets']
+      :repo => node['jenkins']['server']['job-dsl']['repo'],
+      :branch => node['jenkins']['server']['job-dsl']['branch'],
+      :targets => node['jenkins']['server']['job-dsl']['targets']
     }
   )
 end
