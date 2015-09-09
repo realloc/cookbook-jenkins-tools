426a953920892dd04de14542d76e9b508ab09e0c
Stanislav Bogatyrev <realloc@realloc.spb.ru>
2015-09-09 18:00:09 +0300

    Add seed-job

---
+++ attributes/job-dsl.rb
@@ -0,0 +1,4 @@
+default['jenkins-tools']['job-dsl']['seed_job_file'] = 'job-dsl_seed.xml'
+default['jenkins-tools']['job-dsl']['repo'] = 'https://bitbucket.org/realloc_spb_ru/jenkins-jobs.git'
+default['jenkins-tools']['job-dsl']['branch'] = '*/master'
+default['jenkins-tools']['job-dsl']['targets'] = '*.job'
--- recipes/job-dsl.rb
+++ recipes/job-dsl.rb
@@ -3,3 +3,23 @@
 # Recipe:: job-dsl
 
 # Create DSL seed job
+
+seed_job_xml = File.join(
+  Chef::Config[:file_cache_path],
+  node['jenkins-tools']['job-dsl']['seed_job_file']
+)
+
+template seed_job_xml do
+  source 'seed-job.xml.erb'
+  variables(
+    {
+      :repo => node['jenkins-tools']['job-dsl']['repo'],
+      :branch => node['jenkins-tools']['job-dsl']['branch'],
+      :targets => node['jenkins-tools']['job-dsl']['targets']
+    }
+  )
+end
+
+jenkins_job 'Job DSL seed' do
+  config seed_job_xml
+end
